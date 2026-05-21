c4d90ea8a0f5341baa48440f5d83db11db9c7159
GLXB <ustynow.gleb@gmail.com>
2026-05-21 03:35:48 +0300

    ref: Demoved Choice model's dedicated admin page, added Choice as an TabularInline edit in the Question edit page.

--- polls/admin.py
+++ polls/admin.py
@@ -3,12 +3,18 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"]})
     ]
+    inlines = [ChoiceInline]
 
 
-admin.site.register(Choice)
+# admin.site.register(Choice)
 admin.site.register(Question, QuestionAdmin)
