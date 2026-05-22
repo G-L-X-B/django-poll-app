a11dcde859eb8dd1d2b082d0068c2bc098dfec4e
GLXB <ustynow.gleb@gmail.com>
2026-05-22 03:30:07 +0300

    feat: Installed django-debug-toolbar.

--- djangopoll/settings.py
+++ djangopoll/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +123,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Django Debug Toolbar
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
